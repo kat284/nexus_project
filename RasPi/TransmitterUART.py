cdbd25d055b5babaa3b6afd77dc7add85a417898
kat284 <scokie@rocketmail.com>
2016-01-01 19:51:17 -0500

    My First Raspberry Pi 2 Files

---
+++ RasPi/TransmitterUART.py
@@ -0,0 +1,8 @@
+import serial
+from time import sleep
+port = serial.Serial("/dev/ttyAMA0", baudrate=115200, timeout=None)
+
+while 1:
+    pin = raw_input("Activate pin: ").strip("\r\n")
+    port.write(pin)
+    sleep(0.1)
